519c9c6a0de3db2ec7ba04dc79fd43af4c3b8cd9
Anastasia Novikova <anastasizzzs10@gmail.com>
2025-01-04 21:04:02 +0300

    делегаты readOnly ReadWriteOnly

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,95 @@
 package org.example
 
 import transformersFunction
+import kotlin.properties.ReadOnlyProperty
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    var name by LoggingDelegate("Default Name")
+    var age by LoggingDelegate(0)
+
+    val smth by lazy {
+        mutableMapOf(1 to 1)
     }
 
-    transformersFunction()
+
+
+    val text = TranslatedText()
+    text.text = "Hello, world!"
+    println(text.text)
+    smth[2] = 2
+
+    name = "Hello"
+    age +=10
+
+    val computation = ExpensiveComputation()
+    println(computation.result)
+    println(computation.result)
+
+//    transformersFunction()
 }
+
+class TranslatedText {
+    var text: String by TranslateDelegate("en")
+}
+
+class ExpensiveComputation {
+    val result: Int by CacheDelegate { computeSomethingExpensive() }
+
+    private fun computeSomethingExpensive(): Int {
+        println("Computing...")
+        return (1..100).sum()
+    }
+}
+
+
+
+class LoggingDelegate<T> (
+    private var value: T
+): ReadWriteProperty<Any?, T> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return value
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        println("propertie: ${property.name}, old value: ${this.value}")
+        this.value = value
+    }
+
+}
+
+class CacheDelegate<T>(
+    private var value: T? = null,
+    private val action: () -> T) : ReadOnlyProperty<Any?, T?>
+{
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        if (value == null) {
+            value = action.invoke()
+            return value!!
+        } else return value!!
+    }
+
+}
+
+@Suppress("UNCHECKED_CAST")
+class TranslateDelegate<T>(
+    private var value: T
+): ReadWriteProperty<Any,T> {
+    override fun getValue(thisRef: Any, property: KProperty<*>): T {
+        //здесь условно имитация перевода текста
+        return "$value + in English" as T
+    }
+
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
+        this.value = value
+    }
+
+
+}
+
 
 
 
